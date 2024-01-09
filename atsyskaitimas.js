87b4b59d299e67bf30ac9cc60430d675cf70b6ba
zilvinas <zilvis80@mail.ru>
2024-01-09 23:14:35 +0200

    first commit

---
+++ atsyskaitimas.js
@@ -0,0 +1,9 @@
+let DarboDiena = 8;
+let KepaluValanda = 32;
+let DarboJega = 6;
+let Savikaina = 0.5;
+let Kaina = 1.95;
+let UzsaKepalu = 275;
+
+let dienosKiekis = DarboDiena * KepaluValanda * DarboJega;
+console.log('kepyklos Kepalu skaicius:', dienosKiekis, 'per diena');
---
+++ ciklas.js
@@ -0,0 +1,26 @@
+// for (let i = 0; i <= 10; i++) {
+//   console.log(i);
+// }
+// for (let i = 0; i <= 15; i += 2) {
+//   console.log('eiliskumas:', i);
+// }
+// for (let i = 1; i <= 20; i += 3) {
+//   console.log('kas trecias:', [i]);
+// }
+// for (let i = 1; i <= 20; i++) {
+//   if (i % 4 == 0) {
+//     console.log('skaicius:', i, 'dalinasi is 4');
+//   }
+// }
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i);
+// }
+
+let pradzia = 2;
+let pabaiga = 20;
+
+for (let i = pradzia; i <= pabaiga; i++) {
+  if (i % 8 == 0) {
+    console.log(i);
+  }
+}
